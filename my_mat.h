3fbf4bcab744c7eee0504f9835fb1bb6263f066f
yehonatanBar61 <93926764+yehonatanBar61@users.noreply.github.com>
2021-12-02 02:06:43 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,9 @@
+#ifndef MY_MAT_H
+#define MY_MAT_H
+
+int There_is_route(int matrix[10][10], int source, int destenation);
+int min(int x, int y);
+int the_shortest(int matrix[10][10], int x, int y);
+void change_values(int matrix[10][10]);
+
+#endif
